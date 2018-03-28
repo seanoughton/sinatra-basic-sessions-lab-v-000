422b1fbff0a9980386d6e2a95b092e8406902f4f
Sean Oughton <seanoughton@gmail.com>
2018-03-28 17:52:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,9 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-    #The controller action /checkout, should take the params from the form and add it to the session hash. The key should be item and the value should be the item the user entered to the purchase. Make sure to store the session hash in an instance variable that you can access in the views.
+    #The controller action /checkout, should take the params from the form and add it to the session hash.
+    #The key should be item and the value should be the item the user entered to the purchase.
+    #Make sure to store the session hash in an instance variable that you can access in the views.
   end
 
 
